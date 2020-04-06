1d653b718d3b53455d84a2aa166d564701cfd559
Yazmin Pedraza <yazminpedraza.it@gmail.com>
2020-04-05 19:55:03 -0500

    Adding getVideo action and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,5 +21,10 @@ export const logoutRequest = payload =>  ({
 
 export const registerRequest = payload => ({
     type: 'REGISTER_REQUEST',
-    payload
+    payload,
 });
+
+export const getVideoSource = payload => ({
+    type: 'GET_VIDEO_SOURCE',
+    payload,
+})
